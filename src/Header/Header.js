89f66f3fddb20978e4eb48ea11ebee1dc09160e6
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-10 15:56:04 +0300

    Adding a header component

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h1> Simple React ToDo</h1>
+
+        <p>Number of TOdOs </p>
+
+    </div>
+  )
+}
+
+export default Header
