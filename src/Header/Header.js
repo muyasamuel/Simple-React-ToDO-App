b10ead60ef8cd6f7facee49a318b75eac5aa398c
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-11 08:59:40 +0300

    finding and rendering the no of items in todos array

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodoContext } from '../Todo-context/TodoContext';
 import classes from './Header.module.css'
 
 function Header() {
+  const [todos] = useContext(TodoContext);
+
+  const todosNumber = todos.length;
   return (
     <div className={classes.header}>
         <h1> Simple React ToDo</h1>
 
         <div>
-        <p>Number of TOdOs :  <span>10</span> </p>
+        <p>Number of TOdOs :  <span>{todosNumber}</span> </p>
         
         </div>
 
