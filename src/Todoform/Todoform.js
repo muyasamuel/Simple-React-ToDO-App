0237ba35876c420e70c5df4c692ba8ede808a525
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-11 08:26:01 +0300

    state managing the input values

--- src/Todoform/Todoform.js
+++ src/Todoform/Todoform.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Todoform.module.css'
 
 function Todoform() {
+  const [todo , setTodo ] = useState("");
+
+
+  const todoChangeHandler = (e) => {
+    setTodo(e.target.value);
+
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+
+    setTodo("");
+
+  }
+
+
   return (
-    <form className={classes.form}>
-        <input type='text'  placeholder='Add your todo'/>
+    <form className={classes.form} onSubmit={onSubmit}>
+        <input type='text'  placeholder='Add your todo' value={todo} onChange={todoChangeHandler}/>
         <button className={classes.actions}>Add Todo</button>
     </form>
  
